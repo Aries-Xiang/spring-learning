63e1bf161c2eb5f4b7fdabea3d9e11c012a00008
Aries <18182556558@163.com>
2020-03-30 00:43:23 +0800

    20200330 second commit

--- src/test/java/com/aries/springlearning/SpringLearningApplicationTests.java
+++ src/test/java/com/aries/springlearning/SpringLearningApplicationTests.java
@@ -1,13 +1,40 @@
 package com.aries.springlearning;
 
+import com.aries.controller.HelloController;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringLearningApplicationTests {
 
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
     @Test
     void contextLoads() {
+        try {
+            mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(equalTo("Hello World")));
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 
 }
